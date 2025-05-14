1412c9b26a0cd6f8aa2f8e5828498cd8d40be2c8
ki9-feb11 <kara.kiku0211@gmial.com>
2025-05-14 05:14:56 +0000

    [Start]4-8

---
+++ db/migrate/20250514045644_create_post_images.rb
@@ -0,0 +1,14 @@
+class CreatePostImages < ActiveRecord::Migration[6.1]
+  def change
+    create_table :post_images do |t|
+
+      t.integer :id
+      t.string :shop_name
+      t.text :shop_name
+      t.integer :user_id
+
+
+      t.timestamps
+    end
+  end
+end
